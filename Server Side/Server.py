b44b91b8a02868ef9074039d9d784c7dcf33ae81
Arye Kogan <aryekogan@gmail.com>
2017-01-28 18:26:02 +0200

    Renaming functions and variables to pep8 code convenction

--- Server Side/Server.py
+++ Server Side/Server.py
@@ -1,124 +1,133 @@
 from socket import *
-import mysql.connector as dbConnector
+import mysql.connector as db_connector
 import cgi
 import json
 import sys
 
+
 class Server:
-	
-	def __init__(self, port):
-	   
-		self.host = '127.0.0.1'
-		self.port = port
-		self.socket = socket(AF_INET, SOCK_STREAM)
-		self.socket.bind((self.host, self.port))
-		self.socket.listen(10)
-		self.acceptConnections()
+    
+    def __init__(self, port):
 
-	def acceptConnections(self):
-		while True:
-			print("Listening for connections, PORT: ", self.port)
-			client, address = self.socket.accept()
+        self.host = '127.0.0.1'
+        self.port = port
+        self.socket = socket(AF_INET, SOCK_STREAM)
+        self.socket.bind((self.host, self.port))
+        self.socket.listen(10)
+        self.accept_connections()
 
-			print("*********Client sends data*********")
-			print("Loading...")
-			data = self.getData(client, "JSON")
-			print("*****Recieving ended naturally*****")
+    def accept_connections(self):
+        while True:
+            print("Listening for connections, PORT: ", self.port)
+            client, address = self.socket.accept()
 
-			self.updateDB(data)
+            print("*********Client sends data*********")
+            print("Loading...")
+            data = self.get_data(client, "JSON")
+            print("*****Recieving ended naturally*****")
 
-	def getData(self, client, type):
+            self.update_db(data)
 
-		if type == "JSON":
-			stringJSON = self.receiveJsonAsString(client)
-			return self.extacrtJsonObject(stringJSON)
-		else:
-			print("File handling")
-			data = 0; #temp name
-			#return self.receiveFile(client)
-		return -1
+    def get_data(self, client, type_of_data):
 
-	def receiveJsonAsString(self, client):
-		data = client.recv(1024)
-		stringJSON = ''
-		while data:
-			stringJSON += data.decode()
-			data = client.recv(1024)	
-		
-		return stringJSON
-	
-	def receiveFile(self, client):
-		newFile = open("newFile.png", "wb")
-		data = client.recv(1024)
-		while data:
-			newFile.write(data)
-			data = client.recv(1024)
+        if type_of_data == "JSON":
+            string_json = self.receive_json_as_string(client)
+            return self.extract_json_object(string_json)
+        else:
+            print("File handling")
+            #data = 0;  # temp name
+        # return self.receiveFile(client)
+        return -1
 
-		newFile.close()
+    @staticmethod
+    def receive_json_as_string(client):
+        data = client.recv(1024)
+        string_json = ''
+        while data:
+            string_json += data.decode()
+            data = client.recv(1024)
 
-	def extacrtJsonObject(self, stringJSON):
-		jsonArray = json.loads(stringJSON)
-		return JsonObject(jsonArray)
-		
-	def updateDB(self, dataToUpdate):
-		print("Updating DB...")
-		db, cursor = self.connectDB()
-		self.createDB(db, cursor)
-		self.createEntities(db, cursor)
-		self.insertIntoDB(db, cursor, dataToUpdate)
-		cursor.close()
+        return string_json
 
-	#Connect to mySQL DB
-	def connectDB(self):
-		db = dbConnector.connect(host='localhost', user='root', passwd='')
-		cursor = db.cursor()
-		return db, cursor
+    @staticmethod
+    def receive_file(client):
+        new_file = open("new_file.png", "wb")
+        data = client.recv(1024)
+        while data:
+            new_file.write(data)
+            data = client.recv(1024)
 
-	#Create new DB
-	def createDB(self, db, cursor):
-		sql = "create database if not exists LDS"
-		cursor.execute(sql)
-		db.commit()
+        new_file.close()
 
-	#Create a new table into the DB
-	def createEntities(self, db, cursor):
-		cursor.execute("use LDS")
-		sql = '''create table if not exists material
+    @staticmethod
+    def extract_json_object(string_json):
+        json_array = json.loads(string_json)
+        return JsonObject(json_array)
+
+    def update_db(self, data_to_update):
+        print("Updating DB...")
+        db, cursor = self.connect_db()
+        self.create_db(db, cursor)
+        self.create_entities(db, cursor)
+        self.insert_into_db(db, cursor, data_to_update)
+        cursor.close()
+
+    # Connect to mySQL DB
+    @staticmethod
+    def connect_db():
+        db = db_connector.connect(host='localhost', user='root', passwd='')
+        cursor = db.cursor()
+        return db, cursor
+
+    # Create new DB
+    @staticmethod
+    def create_db(db, cursor):
+        sql = "create database if not exists LDS"
+        cursor.execute(sql)
+        db.commit()
+
+    # Create a new table into the DB
+    @staticmethod
+    def create_entities(db, cursor):
+        cursor.execute("use LDS")
+        sql = '''create table if not exists material
 				(materialID int not null auto_increment,
 				material varchar(30) not null,
 				primary key(materialID))'''
-		cursor.execute(sql)
-		db.commit()
-		sql = '''create table if not exists person
+        cursor.execute(sql)
+        db.commit()
+        sql = '''create table if not exists person
 				(personID varchar(10) not null,
 				name varchar(30) not null,
 				primary key(personID))'''
-		cursor.execute(sql)
-		db.commit()
-	
-	#insert values into the DB
-	def insertIntoDB(self, db, cursor, data):
-		sql = "insert into material(material) values('{0}')".format(data.material)
-		cursor.execute(sql)
-		db.commit()
-		sql = "insert into person(personID, name) values('{0}', 'Tomer Achdut')".format(data.suspectId)
-		cursor.execute(sql)
-		db.commit()
+        cursor.execute(sql)
+        db.commit()
+
+    # insert values into the DB
+    @staticmethod
+    def insert_into_db(db, cursor, data):
+        sql = "insert into material(material) values('{0}')".format(data.material)
+        cursor.execute(sql)
+        db.commit()
+        sql = "insert into person(personID, name) values('{0}', 'Tomer Achdut')".format(data.suspectId)
+        cursor.execute(sql)
+        db.commit()
 
 
 class JsonObject:
+    def __init__(self,
+                 json_array):  # dateOfDetection, material, position, suspectId, suspectPlateId, gunId, ramenGraph):
+        self.dateOfDetection = json_array['dateOfDetection']
+        self.material = json_array['material']
+        self.position = json_array['position']
+        self.suspectId = json_array['suspectId']
+        self.suspectPlateId = json_array['suspectPlateId']
+        self.gunId = json_array['gunId']
+        self.ramenGraph = json_array['ramenGraph']
 
-	def __init__(self, jsonArray): #dateOfDetection, material, position, suspectId, suspectPlateId, gunId, ramenGraph):
-		self.dateOfDetection = jsonArray['dateOfDetection']
-		self.material = jsonArray['material']
-		self.position = jsonArray['position']
-		self.suspectId = jsonArray['suspectId']
-		self.suspectPlateId = jsonArray['suspectPlateId']
-		self.gunId = jsonArray['gunId']
-		self.ramenGraph = jsonArray['ramenGraph']
 
 if len(sys.argv) != 2:
-	print("usage: Server.py <port>")
-	exit()
-	
+    print("usage: Server.py <port>")
+    exit()
+
 server = Server(int(sys.argv[1]))
