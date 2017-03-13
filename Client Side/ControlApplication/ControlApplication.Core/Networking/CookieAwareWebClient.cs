a541b55f3ef6b5597a09591e2f487174319e4b4e
Arye Kogan <aryekogan@gmail.com>
2017-03-13 16:41:29 +0200

    Adding CookieAwareWebClient

---
+++ Client Side/ControlApplication/ControlApplication.Core/Networking/CookieAwareWebClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+
+namespace ControlApplication.Core.Networking
+{
+    /// <summary>
+    /// Extending <see cref="WebClient"/> class with cookie container in order to maintain secured sessions 
+    /// </summary>
+    public class CookieAwareWebClient : WebClient
+    {
+        /// <summary>
+        /// The cookie container
+        /// </summary>
+        private readonly CookieContainer mContainer = new CookieContainer();
+
+        /// <summary>
+        /// Overriding this function in order to add or store 
+        /// the cookie (if exists) for each web request.
+        /// </summary>
+        /// <param name="address">Server's address - will be set by <see cref="WebClient"/> public API</param>
+        /// <returns>WebRequest with an embedded cookie</returns>
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            WebRequest request = base.GetWebRequest(address);
+            HttpWebRequest webRequest = request as HttpWebRequest;
+            if (webRequest != null)
+            {
+                webRequest.CookieContainer = mContainer;
+            }
+            return request;
+        }
+    }
+}
