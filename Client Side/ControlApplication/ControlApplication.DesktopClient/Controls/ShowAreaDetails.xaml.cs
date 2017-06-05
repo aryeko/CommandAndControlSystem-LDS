507f21b06867ceb722982978c183efca776edcd8
Arye Kogan <aryekogan@gmail.com>
2017-06-05 17:41:14 +0300

    Adding ShowAreaDetails control

---
+++ Client Side/ControlApplication/ControlApplication.DesktopClient/Controls/ShowAreaDetails.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ControlApplication.Core.Contracts;
+
+namespace ControlApplication.DesktopClient.Controls
+{
+    /// <summary>
+    /// Interaction logic for ShowAreaDetails.xaml
+    /// </summary>
+    public partial class ShowAreaDetails : UserControl
+    {
+        private Area mArea;
+        private IEnumerable<Detection> mDetections; 
+
+        public ShowAreaDetails(Area area, IEnumerable<Detection> detections = null)
+        {
+            InitializeComponent();
+
+            mArea = area;
+            mDetections = detections ?? new List<Detection>();
+            AreaIcon.Source = new BitmapImage(new Uri($"/ControlApplication.DesktopClient;component/Drawable/MapMarker_{mArea.AreaType}.png", UriKind.Relative));
+            LabelHeader.Content = $"{mArea.AreaType} Area";
+            LabelAreaDetails.Content = $"Area Effective Radius: {mArea.Radius} - Area Containes {mDetections.Count()} Detections";
+            this.StackPanel.Children.Add(new ShowMarkerDetections(mDetections));
+        }
+    }
+}
