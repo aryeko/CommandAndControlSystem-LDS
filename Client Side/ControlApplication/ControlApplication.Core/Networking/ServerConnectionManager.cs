fdc311bbeaa5ef389235363d237c9bb47811ea10
Tomer Achdut <tomera90@gmail.com>
2017-06-05 14:04:30 +0300

    Added proxy server design pattern

--- Client Side/ControlApplication/ControlApplication.Core/Networking/ServerConnectionManager.cs
+++ Client Side/ControlApplication/ControlApplication.Core/Networking/ServerConnectionManager.cs
@@ -21,7 +21,7 @@ namespace ControlApplication.Core.Networking
     /// A class that manages the Server connection and creates a single instance of <see cref="ServerConnectionManager"/> (Singelton)
     /// The class provides a detailed API for sessioned communication with the Server.
     /// </summary>
-    public class ServerConnectionManager : INtServerApi, IDisposable
+    internal class ServerConnectionManager : INtServerApi, IDisposable
     {
         /// <summary>
         /// The Remote Server Path
@@ -74,22 +74,11 @@ namespace ControlApplication.Core.Networking
         public List<Material> GetMaterial(string materialId = "", string name = "")
         {
             var materials = new List<Material>();
-            dynamic response;
-            if (!string.IsNullOrEmpty(name))
-            {
-                response = GetCachedObject("material", "material_name", name);
-            }
-            else if(!string.IsNullOrEmpty(materialId))
-            {
-                response = GetCachedObject("material", "_id", materialId);
-            }
-            else
-                response = GetFromDb("material");
+            dynamic response = GetObject("material");
 
             foreach (dynamic obj in response)
             {
-                var materialType = (MaterialType) System.Enum.Parse(typeof(MaterialType), obj.type.ToString());
-                materials.Add(new Material(obj.name.ToString(), materialType, obj.cas.ToString()));
+                materials.Add(ServerObjectConverter.ConvertMaterial(obj));
             }
            
             return materials;
@@ -102,12 +91,11 @@ namespace ControlApplication.Core.Networking
         public List<Area> GetArea(string areaId = "")
         {
             var areas = new List<Area>();
-            dynamic response = !string.IsNullOrEmpty(areaId) ? GetCachedObject("area", "_id", areaId) : GetFromDb("area");
+            dynamic response = GetObject("area");
 
             foreach (dynamic obj in response)
             {
-                var areaType = (AreaType)System.Enum.Parse(typeof(AreaType), obj.area_type.ToString());
-                areas.Add(new Area(ParseLocation(obj.root_location.ToString()), areaType, double.Parse(obj.radius.ToString())));
+               areas.Add(ServerObjectConverter.ConvertArea(obj));
             }
 
             return areas;
@@ -118,14 +106,17 @@ namespace ControlApplication.Core.Networking
         /// </summary>
         /// <param name="gscanId">Filter the search by G-Scan uniqe ID</param>
         /// <returns></returns>
-        public string GetGscan(string gscanId)
+        public List<string> GetGscan(string gscanId = "")
         {
-            if (gscanId.Equals("no gscan"))
-                return "";
+            var gscans = new List<string>();
+            var response = GetObject("gscan");
 
-            dynamic response = GetCachedObject("gscan", "_id", gscanId);
+            foreach (dynamic obj in response)
+            {
+                gscans.Add(obj[0].gscan_sn.ToString());
+            }
 
-            return response[0].gscan_sn.ToString();
+            return gscans;
         }
 
         /// <summary>
@@ -134,23 +125,7 @@ namespace ControlApplication.Core.Networking
         /// <returns></returns>
         public List<Detection> GetDetections()
         {
-            var detectionsList = new List<Detection>();
-            dynamic response = GetFromDb("detection");
-
-            foreach (dynamic obj in response)
-            {
-                DateTime dateTime = DateTime.ParseExact(obj.date_time.ToString(), "G", CultureInfo.InvariantCulture);
-                var position = ParseLocation(obj.location.ToString());
-                string gscanSn = GetGscan(obj.gscan_id.ToString());
-                string ramanOutput = GetRaman(obj.raman_output_id.ToString());
-                var area = GetArea(obj.area_id.ToString());
-                var material = GetMaterial(materialId:obj.material_id.ToString());
-                var detection = new Detection(dateTime, material[0], position, area[0], obj.suspect_id.ToString(), obj.plate_number.ToString(), gscanSn, ramanOutput);
-                
-                detectionsList.Add(detection);
-            }
-
-            return detectionsList;
+            return null;
         }
 
         /// <summary>
@@ -164,38 +139,24 @@ namespace ControlApplication.Core.Networking
                 return "";
 
             //TODO: handle a real raman link
-            //dynamic response = GetCachedObject("raman", "_id", ramanOutput);
+            //dynamic response = GetObject("raman", "_id", ramanOutput);
             //return response[0]._id.ToString();
             return "";  
-        }
-
-        /// <summary>
-        /// Parse a location string from a JSON string
-        /// </summary>
-        /// <param name="location">The location </param>
-        /// <returns>New PointLatLng using the parsed Lat and Lng</returns>
-        private PointLatLng ParseLocation(string location)
-        {
-            var pointPattern = @"{Lat=(?<lat>\d+\.*\d*?),\sLng=(?<lng>\d+\.*\d*?)}";
-
-            var locationResult = Regex.Match(location, pointPattern);
-            
-            return new PointLatLng(double.Parse(locationResult.Groups["lat"].Value), double.Parse(locationResult.Groups["lng"].Value));
         }
 
         /// <summary>
         /// Adds a detection to the database using server's RESTful API
         /// </summary>
         /// <param name="detection">A detection to add</param>
-        public void AddDetection(Detection detection)
-        {
-            var ids = GetAllDbIds(detection);
+        /// <param name="idsDictionary">Dictionary of IDs</param>
+        public void AddDetection(Detection detection, Dictionary<string,string> idsDictionary)
+        {    
             var postData = new NameValueCollection
             {
-                { "user_id", ids["UserId"] },
-                { "material_id", ids["MaterialId"] },
-                { "area_id", ids["AreaId"] },
-                { "gscan_id", ids["GscanId"] },               
+                { "user_id", idsDictionary["UserId"] },
+                { "material_id", idsDictionary["MaterialId"] },
+                { "area_id", idsDictionary["AreaId"] },
+                { "gscan_id", idsDictionary["GscanId"] },               
                 { "suspect_id", detection.SuspectId },
                 { "raman_id", detection.RamanId },
                 { "plate_number", detection.SuspectPlateId },
@@ -216,57 +177,10 @@ namespace ControlApplication.Core.Networking
             {
                 { "area_type", newArea.AreaType.ToString() },
                 { "root_location", newArea.RootLocation.ToString() },
-                { "radius", newArea.Radius.ToString() },
+                { "radius", newArea.Radius.ToString() }
             };
 
             PostToDb("area", postData);
-        }
-
-        /// <summary>
-        /// Gets all the IDs from a detection in order to add a detection to DB using forein keys
-        /// </summary>
-        /// <param name="detection">The detection to add</param>
-        /// <returns></returns>
-        private Dictionary<string, string> GetAllDbIds(Detection detection)
-        {
-            var ids = new Dictionary<string, string>();
-
-            dynamic response = GetCachedObject("material", "material_name", detection.Material.Name);
-            ids.Add("MaterialId", response[0]._id.ToString());
-
-            response = GetCachedObject("area", "root_location", $"[{detection.Area.RootLocation.Lat},{detection.Area.RootLocation.Lng}]");
-            ids.Add("AreaId", response[0]._id.ToString());
-
-            if (string.IsNullOrEmpty(detection.GunId))
-                ids.Add("GscanId", "");
-            else
-            {
-                response = GetCachedObject("gscan", "gscan_sn", detection.GunId);
-                ids.Add("GscanId", response[0]._id.ToString());
-            }
-            
-            response = GetCachedObject("user", "username", "lds");
-            ids.Add("UserId", response[0]._id.ToString());
-
-            return ids;
-        }
-
-        /// <summary>
-        /// A generic method for getting and setting objects to the memory cache.
-        /// </summary>
-        /// <param name="uriPath">URI Path</param>
-        /// <param name="key">The key to get value from</param>
-        /// <param name="value">The key value</param>
-        /// <returns>An object of type dynamic</returns>
-        private dynamic GetCachedObject(string uriPath, string key = "", string value = "")
-        {
-            var response = CacheManager.GetObjectFromCache<dynamic>(value);
-            if (response == null)
-            {
-                response = GetFromDb(uriPath, key, value);
-                CacheManager.SetObjectToCache(value, response);
-            }
-            return response;
         }
 
         /// <summary>
@@ -276,7 +190,7 @@ namespace ControlApplication.Core.Networking
         /// <param name="key">The key to get value from</param>
         /// <param name="value">The key value</param>
         /// <returns>Response from DB</returns>
-        private dynamic GetFromDb(string uriPath, string key = "", string value = "")
+        public dynamic GetObject(string uriPath, string key = "", string value = "")
         {
             if (!string.IsNullOrEmpty(key) && !string.IsNullOrEmpty(value))
             {
