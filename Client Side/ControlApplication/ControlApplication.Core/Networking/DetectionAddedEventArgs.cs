dcee86bfc146dc11c4335519a59eeb1ed73ad30e
Arye Kogan <aryekogan@gmail.com>
2017-06-13 09:50:26 +0300

    Adding DetectionAddedEventArgs

---
+++ Client Side/ControlApplication/ControlApplication.Core/Networking/DetectionAddedEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ControlApplication.Core.Contracts;
+
+namespace ControlApplication.Core.Networking
+{
+    public class DetectionAddedEventArgs : EventArgs
+    {
+        public DetectionAddedEventArgs(Detection detection)
+        {
+            Detection = detection;
+        }
+
+        public Detection Detection { get; }      
+    }
+}
