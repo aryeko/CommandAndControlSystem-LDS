626b94d28db3c5bb17bcfa1c383b7ea97eff7551
Arye Kogan <aryekogan@gmail.com>
2017-06-13 09:54:23 +0300

    Ading on detection added functionallity to AlertsManager

--- Client Side/ControlApplication/ControlApplication.Core/AlertsManager.cs
+++ Client Side/ControlApplication/ControlApplication.Core/AlertsManager.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ControlApplication.Core.Contracts;
+using ControlApplication.Core.Networking;
 
 namespace ControlApplication.Core
 {
@@ -28,6 +29,31 @@ namespace ControlApplication.Core
         public delegate void CombinationFoundAlertHandler(object source, CombinationAlertArgs args);
 
         public static event CombinationFoundAlertHandler CombinationFoundAlert;
+
+        static AlertsManager()
+        {
+            NetworkClientsFactory.GetNtServer().DetectionAdded += OnDetectionAdded;
+        }
+
+        private static void OnDetectionAdded(object sender, DetectionAddedEventArgs e)
+        {
+            var affectedAreaDetections = NetworkClientsFactory.GetNtServer().GetDetections(e.Detection.Area.DatabaseId);
+            var affectedAreaMaterials = affectedAreaDetections.Select(d => d.Material).ToList();
+
+            var combinations = NetworkClientsFactory.GetNtServer().GetMaterialsCombinationsAlerts();
+
+            foreach (var combination in combinations)
+            {
+                if (combination.ContainesCombination(affectedAreaMaterials))
+                {
+                    var alertedDetections =
+                        affectedAreaDetections.GroupBy(d => d.Material)
+                            .Select(
+                                group => combination.CombinationMaterialsList.Contains(group.Key) ? group.Last() : null).ToList();
+                    AlertSystem(sender, e.Detection.Area, alertedDetections);
+                }
+            }
+        }
 
         public static void AlertSystem(object source, Area area, List<Detection> detections)
         {
